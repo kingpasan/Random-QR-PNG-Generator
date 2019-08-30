97a5f1ce96e28a100a27db4e3e085d5732bcff74
Pasan Semage <pmsemage@gmail.com>
2019-08-31 02:04:16 +0530

    Add project files.

---
+++ QRCodeGenarator/frmRandomQRGenarator.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace QRCodeGenarator
+{
+    partial class frmRandomQRGenarator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenarate = new System.Windows.Forms.Button();
+            this.pgrBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCompleted = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenarate
+            // 
+            this.btnGenarate.Location = new System.Drawing.Point(11, 175);
+            this.btnGenarate.Name = "btnGenarate";
+            this.btnGenarate.Size = new System.Drawing.Size(431, 44);
+            this.btnGenarate.TabIndex = 0;
+            this.btnGenarate.Text = "Genarate Random QR Codes";
+            this.btnGenarate.UseVisualStyleBackColor = true;
+            this.btnGenarate.Click += new System.EventHandler(this.btnGenarate_Click);
+            // 
+            // pgrBar
+            // 
+            this.pgrBar.Location = new System.Drawing.Point(11, 119);
+            this.pgrBar.Name = "pgrBar";
+            this.pgrBar.Size = new System.Drawing.Size(431, 23);
+            this.pgrBar.Step = 1;
+            this.pgrBar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Saving QR PNG to : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Completed :";
+            // 
+            // lblCompleted
+            // 
+            this.lblCompleted.AutoSize = true;
+            this.lblCompleted.Location = new System.Drawing.Point(97, 145);
+            this.lblCompleted.Name = "lblCompleted";
+            this.lblCompleted.Size = new System.Drawing.Size(0, 17);
+            this.lblCompleted.TabIndex = 4;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(11, 39);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(431, 22);
+            this.txtPath.TabIndex = 5;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(313, 67);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(129, 33);
+            this.btnSelectFolder.TabIndex = 6;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 16;
+            this.list.Location = new System.Drawing.Point(11, 237);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(428, 308);
+            this.list.TabIndex = 7;
+            this.list.SelectedIndexChanged += new System.EventHandler(this.list_SelectedIndexChanged);
+            // 
+            // frmRandomQRGenarator
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(451, 553);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblCompleted);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pgrBar);
+            this.Controls.Add(this.btnGenarate);
+            this.Name = "frmRandomQRGenarator";
+            this.Text = "Random QR Genarator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenarate;
+        private System.Windows.Forms.ProgressBar pgrBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCompleted;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.ListBox list;
+    }
+}
+
---
+++ QRCodeGenarator/frmRandomQRGenarator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using ZXing;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Collections;
+
+namespace QRCodeGenarator
+{
+    public partial class frmRandomQRGenarator : Form
+    {
+
+        ArrayList randomArr = new ArrayList();
+
+        public frmRandomQRGenarator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenarate_Click(object sender, EventArgs e)
+        {
+
+            string path = txtPath.Text;
+
+            if (path.Length > 0)
+            {
+
+                for (int i = randomArr.Count; i < 100; i++)
+                {
+                    string randomString = Guid.NewGuid().ToString("n").Substring(0, 10);
+
+                    if (!randomArr.Contains(randomString))
+                    {
+                        randomArr.Add(randomString);
+                        list.Items.Add(randomString);
+                    }
+                }
+
+                for (int a = 0; a < randomArr.Count; a++)
+                {
+                    int count = a + 1;
+                    pgrBar.Value = count;
+                    lblCompleted.Text = count.ToString();
+
+                    genarateQRCode(randomArr[a].ToString(), path + "\\" + randomArr[a].ToString()+ ".png");
+                }
+
+                MessageBox.Show(this, "Task Completed!" , "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtPath.Text = "";
+                pgrBar.Value = 0;
+                lblCompleted.Text = "";
+            }
+            else
+            {
+                MessageBox.Show(this, "Please Select Folder Path", "Path Empty", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void genarateQRCode(string value, string filename)
+        {
+
+            
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+            barcodeWriter.Options.Height = 256;
+            barcodeWriter.Options.Width = 256;
+
+            barcodeWriter.Write(value).Save(filename);
+
+            
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+
+            dialog.InitialDirectory = "C:\\";
+
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtPath.Text = dialog.FileName;
+            }
+        }
+
+        private void list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
